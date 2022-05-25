2eb71084bccaf54f19a5bc3775a5b31a91977a6d
Grant Swertfeger <grant@swordsweeper.com>
2022-05-24 23:03:02 -0500

    Updated structure and versions

---
+++ index.js
@@ -0,0 +1,13 @@
+/*!
+ * developer-utils <https://github.com/swowrdsweeper/developer-utils>
+ *
+ * Copyright (c) 2022 Swordweeper Industries, LLC.
+ * Licensed under the MIT license.
+ */
+'use strict';
+const numeric = require('./lib/numeric');
+const utils = {
+    numeric,
+}
+
+module.exports = utils;
